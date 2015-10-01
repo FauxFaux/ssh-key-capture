720b1ce4bcbad862308ad76bace3b5818ac536f7
Chris West (Faux) <git@goeswhere.com>
2015-10-01 23:59:07 +0100

    new user mechanics

--- src/main/java/com/goeswhere/sshkeycapture/KeyCapture.java
+++ src/main/java/com/goeswhere/sshkeycapture/KeyCapture.java
@@ -1,23 +1,21 @@
 package com.goeswhere.sshkeycapture;
 
-import org.apache.sshd.common.Factory;
 import org.apache.sshd.common.keyprovider.AbstractKeyPairProvider;
 import org.apache.sshd.common.session.Session;
 import org.apache.sshd.common.util.SecurityUtils;
-import org.apache.sshd.server.Command;
 import org.apache.sshd.server.SshServer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.InputStreamReader;
 import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
 import java.security.GeneralSecurityException;
 import java.security.KeyPair;
 import java.security.PublicKey;
 import java.util.*;
-import java.util.concurrent.CountDownLatch;
 
 public class KeyCapture {
     public static final Session.AttributeKey<String> ACCOUNT_NAME = new Session.AttributeKey<>();
@@ -27,21 +25,19 @@ public class KeyCapture {
 
     public static void main(String[] args) throws IOException, InterruptedException {
         SshServer sshd = SshServer.setUpDefaultServer();
-        final CountDownLatch readyToShutdown = new CountDownLatch(1);
 
         final KeyPair serverKeyPair = serverKeyPair();
         final Map<String, String> userDatabase = new HashMap<>();
-
-        final String adminKey = UUID.randomUUID().toString();
-        System.out.println(adminKey);
+        final Map<String, String> issuedTokens = new HashMap<>();
 
         sshd.setPort(9422);
 
         sshd.setPublickeyAuthenticator((username, key, session) -> {
-            if (username.startsWith(adminKey)) {
-                final String newUser = username.substring(adminKey.length() + 1);
+            final String newUser = issuedTokens.get(username);
+            if (null != newUser) {
                 userDatabase.put(newUser, fingerprint(key));
                 session.setAttribute(JUST_ADDED, true);
+                session.setAttribute(ACCOUNT_NAME, username);
                 return true;
             }
 
@@ -68,18 +64,35 @@ public class KeyCapture {
 
         sshd.setShellFactory(CommandWrapper.wrap((in, out, err, session) -> {
             try (final PrintStream ps = new PrintStream(out)) {
+                final String whom = session.getAttribute(ACCOUNT_NAME);
+
                 if (session.getAttribute(JUST_ADDED)) {
                     ps.println("Added successfully!  You can now log-in normally.\r");
+                    issuedTokens.remove(whom);
                     return 0;
                 }
-                ps.println("Hi!  You've successfully authenticated as " + session.getAttribute(ACCOUNT_NAME) + "\r");
+                ps.println("Hi!  You've successfully authenticated as " + whom + "\r");
                 ps.println("Bye!\r");
             }
             return 0;
         }));
 
         sshd.start();
-        readyToShutdown.await();
+
+            try (final BufferedReader stdin = new BufferedReader(new InputStreamReader(System.in, StandardCharsets.UTF_8))) {
+                while (true) {
+                    System.out.print("Enter a new user name, or blank to exit: ");
+                    final String user = stdin.readLine().trim();
+                    if (user.isEmpty()) {
+                        return;
+                    }
+
+                    final String newUuid = UUID.randomUUID().toString();
+                    issuedTokens.put(newUuid, user);
+
+                    System.out.println("Ask '" + user + "' to ssh to '" + newUuid + "@...'");
+                }
+        }
     }
 
     private static KeyPair serverKeyPair() {
