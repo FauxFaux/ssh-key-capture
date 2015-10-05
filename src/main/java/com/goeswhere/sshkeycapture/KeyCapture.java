fc917d405649dfa9f409cbfe0626e007332901ae
Chris West (Faux) <git@goeswhere.com>
2015-10-05 21:14:07 +0100

    An example of capturing all the keys using keyboard-interactive as a fallback.
    Totally not fully wired up.
    
    % ssh -p 9422 c4dc5895-3f32-4124-87e1-a28024be53e7@localhost
    Password authentication
    Pick some keys:
    1) RSA MIIBIjANBgkqh..
    2) EC MFkwEwYHKoZIzj..
    
    Your choice (e.g. '1 2'):
    
    And the server logs:
    INFO Server session created from /0:0:0:0:0:0:0:1:40120
    keys to use:
    EC MFkwEwYHKoZIzj..

--- src/main/java/com/goeswhere/sshkeycapture/KeyCapture.java
+++ src/main/java/com/goeswhere/sshkeycapture/KeyCapture.java
@@ -4,6 +4,10 @@ import org.apache.sshd.common.keyprovider.AbstractKeyPairProvider;
 import org.apache.sshd.common.session.Session;
 import org.apache.sshd.common.util.SecurityUtils;
 import org.apache.sshd.server.SshServer;
+import org.apache.sshd.server.auth.UserAuth;
+import org.apache.sshd.server.auth.UserAuthKeyboardInteractive;
+import org.apache.sshd.server.auth.UserAuthKeyboardInteractiveFactory;
+import org.apache.sshd.server.session.ServerSession;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -14,11 +18,13 @@ import java.security.GeneralSecurityException;
 import java.security.KeyPair;
 import java.security.PublicKey;
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class KeyCapture implements AutoCloseable {
     private static final Logger logger = LoggerFactory.getLogger(KeyCapture.class);
 
     static final Session.AttributeKey<String> ACCOUNT_NAME = new Session.AttributeKey<>();
+    static final Session.AttributeKey<List<String>> COLLECTED_KEYS = new Session.AttributeKey<>();
     static final Session.AttributeKey<Boolean> JUST_ADDED = new Session.AttributeKey<>();
 
     private final SshServer sshd = SshServer.setUpDefaultServer();
@@ -39,10 +45,13 @@ public class KeyCapture implements AutoCloseable {
             synchronized (KeyCapture.this) {
                 final String newUser = issuedTokens.get(username);
                 if (null != newUser) {
-                    userDatabase.put(newUser, fingerprint);
-                    session.setAttribute(JUST_ADDED, true);
-                    session.setAttribute(ACCOUNT_NAME, username);
-                    return true;
+                    List<String> collectedKeys = session.getAttribute(COLLECTED_KEYS);
+                    if (null == collectedKeys) {
+                        session.setAttribute(COLLECTED_KEYS, collectedKeys = new ArrayList<>());
+                    }
+
+                    collectedKeys.add(fingerprint);
+                    return false;
                 }
 
                 expectedKey = userDatabase.get(username);
@@ -59,6 +68,16 @@ public class KeyCapture implements AutoCloseable {
             return true;
         });
 
+        sshd.setUserAuthFactories(Arrays.asList(
+                sshd.DEFAULT_USER_AUTH_PUBLIC_KEY_FACTORY,
+                new UserAuthKeyboardInteractiveFactory() {
+                    @Override
+                    public UserAuth create() {
+                        return new AskAboutTheKeysInteractively();
+                    }
+                }));
+
+
         sshd.setKeyPairProvider(new AbstractKeyPairProvider() {
             @Override
             public Iterable<KeyPair> loadKeys() {
@@ -70,7 +89,7 @@ public class KeyCapture implements AutoCloseable {
             try (final PrintStream ps = new PrintStream(out)) {
                 final String whom = session.getAttribute(ACCOUNT_NAME);
 
-                if (session.getAttribute(JUST_ADDED)) {
+                if (session.getAttribute(JUST_ADDED) == Boolean.TRUE) {
                     ps.println("Added successfully!  You can now log-in normally.\r");
                     synchronized (KeyCapture.this) {
                         issuedTokens.remove(whom);
@@ -113,4 +132,53 @@ public class KeyCapture implements AutoCloseable {
     public Map<String, String> getUsers() {
         return Collections.unmodifiableMap(userDatabase);
     }
+
+    private static class AskAboutTheKeysInteractively extends UserAuthKeyboardInteractive {
+        @Override
+        protected boolean isInteractionPromptEchoEnabled() {
+            return true;
+        }
+
+        @Override
+        protected String getInteractionInstruction() {
+            List<String> collectedKeys = session.getAttribute(COLLECTED_KEYS);
+            if (null == collectedKeys || collectedKeys.isEmpty()) {
+                return "You didn't provide any keys!  You must, you must.";
+            }
+
+            StringBuilder keyList = new StringBuilder("Pick some keys: \n");
+            for (int i = 0; i < collectedKeys.size(); i++) {
+                keyList.append(i+1).append(") ").append(collectedKeys.get(i)).append("\n");
+            }
+
+            return keyList.toString();
+        }
+
+        @Override
+        protected String getInteractionPrompt() {
+            List<String> collectedKeys = session.getAttribute(COLLECTED_KEYS);
+            if (null == collectedKeys || collectedKeys.isEmpty()) {
+                return "Please reconnect and try again; press anything to exit.";
+            }
+
+            return "Your choice (e.g. '1 2'): ";
+        }
+
+        @Override
+        protected boolean checkPassword(ServerSession session, String username, String password) throws Exception {
+            try {
+                final List<Integer> selections = Arrays.asList(password.split("\\s+")).stream()
+                        .map(Integer::parseInt)
+                        .collect(Collectors.toList());
+                List<String> collectedKeys = session.getAttribute(COLLECTED_KEYS);
+                System.out.println("keys to use:");
+                selections.forEach(idx -> System.out.println(collectedKeys.get(idx - 1)));
+                return true;
+            } catch (NumberFormatException e) {
+                logger.info("couldn't parse " + password, e);
+                return false;
+            }
+        }
+    }
+
 }
